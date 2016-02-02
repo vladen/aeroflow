8380d266ba3fd5aef7a3e7410d05816dca812a14
Yuliya Maksimchyk <yuliya_maksimchyk@epam.com>
2016-02-02 13:03:29 +0300

    [*] rebuild aeroflow includes new tests

--- build/legacy/aeroflow.tests.js
+++ build/legacy/aeroflow.tests.js
@@ -1,24 +1,133 @@
 (function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(["module", "exports"], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(module, exports);
-  } else {
-    var mod = {
-      exports: {}
-    };
-    factory(mod, mod.exports);
-    global.aeroflowTests = mod.exports;
-  }
+    if (typeof define === "function" && define.amd) {
+        define(['module', 'exports'], factory);
+    } else if (typeof exports !== "undefined") {
+        factory(module, exports);
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod, mod.exports);
+        global.aeroflowTests = mod.exports;
+    }
 })(this, function (module, exports) {
-  "use strict";
+    'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
 
-  var aeroflow = function aeroflow(_aeroflow, assert) {};
+    var factoryTests = function factoryTests(aeroflow, assert) {
+        return describe('aeroflow', function () {
+            it('is function', function () {
+                assert.isFunction(aeroflow);
+            });
+            describe('aeroflow()', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow(), 'Aeroflow');
+                });
+                describe('#sources', function () {
+                    it('is initially empty array', function () {
+                        assert.strictEqual(aeroflow().sources.length, 0);
+                    });
+                });
+                describe('#emitter', function () {
+                    it('is function', function () {
+                        assert.isFunction(aeroflow().emitter);
+                    });
+                });
+            });
+            describe('aeroflow(@Array)', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow([1, 2]), 'Aeroflow');
+                });
+                describe('@Array', function () {
+                    it('emitting array items', function () {
+                        var expected = ['str', new Date(), {}, 1];
+                        var index = 0;
+                        aeroflow(expected).run(function (value) {
+                            return assert.strictEqual(value, expected[index++]);
+                        });
+                    });
+                });
+            });
+            describe('aeroflow(@Map)', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow(new Map([[1, 2], [2, 1]])), 'Aeroflow');
+                });
+                describe('@Map', function () {
+                    it('emitting map entries', function () {
+                        var expected = [['a', 1], ['b', 2]];
+                        var index = 0;
+                        aeroflow(new Map(expected)).run(function (value) {
+                            return assert.includeMembers(value, expected[index++]);
+                        });
+                    });
+                });
+            });
+            describe('aerflow(@Set)', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow(new Set([1, 2])), 'Aeroflow');
+                });
+                describe('@Set', function () {
+                    it('emitting set keys', function () {
+                        var expected = ['a', 'b'];
+                        var index = 0;
+                        aeroflow(new Set(expected)).run(function (value) {
+                            return assert.strictEqual(value, expected[index++]);
+                        });
+                    });
+                });
+            });
+            describe('aeroflow(@Function)', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow(function () {
+                        return true;
+                    }), 'Aeroflow');
+                });
+                describe('@Function', function () {
+                    it('emitting scalar value returned by function', function () {
+                        var expected = 'test tester';
+                        aeroflow(function () {
+                            return expected;
+                        }).run(function (value) {
+                            return assert.strictEqual(value, expected);
+                        });
+                    });
+                });
+            });
+            describe('Aeroflow(@Promise)', function () {
+                it('returns instance of Aeroflow', function () {
+                    assert.typeOf(aeroflow(Promise.resolve({})), 'Aeroflow');
+                });
+                describe('@Promise', function () {
+                    it('emitting array resolved by promise', function () {
+                        var expected = ['a', 'b'];
+                        aeroflow(Promise.resolve(expected)).run(function (value) {
+                            return assert.strictEqual(value, expected);
+                        });
+                    });
+                    it('emitting scalar value resolved by promise returned by function asynchronously', function () {
+                        var expected = ['a', 'b'];
+                        aeroflow(function () {
+                            return new Promise(function (resolve) {
+                                return setTimeout(function () {
+                                    return resolve(expected);
+                                });
+                            });
+                        }).run(function (value) {
+                            return assert.strictEqual(value, expected);
+                        });
+                    });
+                });
+            });
+        });
+    };
 
-  exports.default = aeroflow;
-  module.exports = exports['default'];
+    var aeroflow = function aeroflow(_aeroflow, assert) {
+        factoryTests(_aeroflow, assert);
+    };
+
+    exports.default = aeroflow;
+    module.exports = exports['default'];
 });
--- build/modern/aeroflow.tests.js
+++ build/modern/aeroflow.tests.js
@@ -1,11 +1,116 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(global.aeroflowTests = factory());
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    (global.aeroflowTests = factory());
 }(this, function () { 'use strict';
 
-	var aeroflow = (aeroflow, assert) => {};
+    var factoryTests = (aeroflow, assert) => describe('aeroflow', () => {
+        it('is function', () => {
+            assert.isFunction(aeroflow);
+        });
 
-	return aeroflow;
+        describe('aeroflow()', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow(), 'Aeroflow');
+            });
+
+            describe('#sources', () => {
+                it('is initially empty array', () => {
+                    assert.strictEqual(aeroflow().sources.length, 0);
+                });
+            });
+
+            describe('#emitter', () => {
+                it('is function', () => {
+                    assert.isFunction(aeroflow().emitter);
+                });
+            });
+        });
+
+        describe('aeroflow(@Array)', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow([1, 2]), 'Aeroflow');
+            });
+
+            describe('@Array', () => {
+                it('emitting array items', () => {
+                    let expected = ['str', new Date(), {}, 1];
+                    let index = 0;
+                    aeroflow(expected)
+                        .run(value => assert.strictEqual(value, expected[index++]));
+                });
+            });
+        });
+
+        describe('aeroflow(@Map)', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow(new Map([[1,2], [2, 1]])), 'Aeroflow');
+            });
+
+            describe('@Map', () => {
+                it('emitting map entries', () => {
+                    let expected = [['a', 1], ['b', 2]];
+                    let index = 0;
+                    aeroflow(new Map(expected))
+                        .run(value => assert.includeMembers(value, expected[index++]));
+                });
+            });
+        });
+
+        describe('aerflow(@Set)', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow(new Set([1, 2])), 'Aeroflow');
+            });
+
+            describe('@Set', () => {
+                it('emitting set keys', () => {
+                    let expected = ['a', 'b'];
+                    let index = 0;
+                    aeroflow(new Set(expected))
+                        .run(value => assert.strictEqual(value, expected[index++]));
+                });
+            });
+        });
+
+        describe('aeroflow(@Function)', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow(() => true), 'Aeroflow');
+            });
+
+            describe('@Function', () => {
+                it('emitting scalar value returned by function', () => {
+                    let expected = 'test tester';
+                    aeroflow(() => expected)
+                        .run(value => assert.strictEqual(value, expected));
+                });
+            });
+        });
+
+        describe('Aeroflow(@Promise)', () => {
+            it('returns instance of Aeroflow', () => {
+                assert.typeOf(aeroflow(Promise.resolve({})), 'Aeroflow');
+            });
+
+            describe('@Promise', () => {
+               it('emitting array resolved by promise', () => {
+                    let expected = ['a', 'b'];
+                    aeroflow(Promise.resolve(expected))
+                        .run( value => assert.strictEqual(value, expected));
+               });
+
+               it('emitting scalar value resolved by promise returned by function asynchronously', () => {
+                    let expected = ['a', 'b'];
+                    aeroflow(() => new Promise(resolve => setTimeout(() => resolve(expected))))
+                        .run(value => assert.strictEqual(value, expected));
+               });
+            });
+        });
+    });
+
+    var aeroflow = (aeroflow, assert) => {
+        factoryTests(aeroflow, assert);
+    };
+
+    return aeroflow;
 
 }));
