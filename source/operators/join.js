6a3b7a3efc7ca2657943df7320e3abaab3c60faa
DzmitryRybalko <Dzmitry_Rybalko@epam.com>
2016-02-04 17:10:58 +0300

    Add adapterEmitter

--- source/operators/join.js
+++ source/operators/join.js
@@ -2,26 +2,27 @@
 
 import { isError } from '../utilites';
 import { arrayEmitter } from '../emitters/array';
+import { adapterEmitter } from '../emitters/adapter';
 import { toArrayOperator } from './toArray';
 
-export function joinOperator(flow, predicate) {
+export function joinOperator(right, predicate) {
+  let array = [], source;
 
   return emitter => (next, done, context) => {
   let source, array = [];
-  toArrayOperator()(emitter)(
+  var rightEmitter = adapterEmitter(right, true);
+  toArrayOperator()(rightEmitter)(
       result => {
         source = result;
         return false;
       },
       result => {        
         if (isError(result)) done(result);
-        else {
-          flow.run(value => {
-              array.push(source
+        else {          
+          context.flow.run(value => {
+              source
                 .filter(x => !predicate || predicate(x, value))
-                .map(x => {
-                  return [x, value];
-                }));
+                .map(x => array.push([x, value]));
               }, 
             () => { 
               arrayEmitter(array)(next, done, context) 
